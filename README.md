b790438510aee837003981083e32ab24b469fcd5
Gabri95 <gabriele.cesa@gmail.com>
2017-10-10 19:52:09 +0200

    parametrized the script

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 Second project for the Knowledge Representation course in the MSc Artificial Intelligence at the University of Amsterdam.
 
 We wrote a Python script which go through the RDF/XML file representing an ontology and try to remove the redundant clasues, employing the HermiT reasoner.
+
+Usage:
+
+python redundancy_checker.py -i <original_ontology> -o <output_file>
+
+where "original_ontology" should be the RDF/XML file containing the original ontology while "output_file" will contain the resulting reduced ontology.
--- redundancy_checker.py
+++ redundancy_checker.py
@@ -8,119 +8,130 @@ import subprocess
 import sys
 from filecmp import cmp
 import io
+import os.path
+import sys, getopt
+
+
 
 
 ns = {'owl': 'http://www.w3.org/2002/07/owl#', 'rdf':'http://www.w3.org/1999/02/22-rdf-syntax-ns#', 'rdfs':'http://www.w3.org/2000/01/rdf-schema#'}
 
 
-orig_file = 'pizza_consistent_rdf.owl'
-
-
-#/home/davide/Jupyter Notebook Projects/DL/
-tree = etree.parse(orig_file)
-
-
-xslt=b'''<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
-<xsl:output method="xml" indent="no"/>
-
-<xsl:template match="/|comment()|processing-instruction()">
-    <xsl:copy>
-      <xsl:apply-templates/>
-    </xsl:copy>
-</xsl:template>
-
-<xsl:template match="*">
-    <xsl:element name="{local-name()}">
-      <xsl:apply-templates select="@*|node()"/>
-    </xsl:element>
-</xsl:template>
-
-<xsl:template match="@*">
-    <xsl:attribute name="{local-name()}">
-      <xsl:value-of select="."/>
-    </xsl:attribute>
-</xsl:template>
-</xsl:stylesheet>
-'''
-IO = io.BytesIO(xslt)
-xslt_doc=etree.parse(IO)
-transform=etree.XSLT(xslt_doc)
-
-#get the root of the XML tree
-root = tree.getroot()
-
-#retrieve the list of subClassOf elements with XPath
-clauses = root.xpath('/rdf:RDF/owl:Class/rdfs:subClassOf', namespaces=ns)
-
-print('There are ' + str(len(clauses)) + ' clauses in the ontology')
-
-
-
-#wrapper for Hermit reasoner to check if the ontology in the first file entails the one in the second
-def check(f1, f2):
-    test = subprocess.Popen(
-        ["java", "-jar", "HermiT.jar",
-         "--premise=file:///home/gabriele/Documents/KR/Assignment_2/" + f1,
-         "--conclusion=file:///home/gabriele/Documents/KR/Assignment_2/" + f2,
-         "-E"],
-        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-    out, err = test.communicate()
-    return out.decode() == 'true\n'
-
-
-#list of redundant clauses
-redundants = []
-
-#iterate through the clauses
-for i, clause in enumerate(clauses):
+def main(argv):
+    
+    orig_file = 'pizza_consistent_rdf.owl'
+    tmp_file = "tmp.owl"
     
     
-    if i% int(len(clauses)/40) == 0:
-        print(str(i/len(clauses)))
-        print(len(redundants))
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:",["input=", "output="])
+
+    except getopt.GetoptError:
+        print ('script.py -i <input> -o <output>')
+        sys.exit(2)
     
-    #retrieve the parent of the subClassOf element, i.e. the subclass in the clause    
-    parent = clause.getparent()
+    for opt, arg in opts:
+        if opt == '-h':
+            print ('script.py -i <input> -o <output>')
+            sys.exit()
+        elif opt in ("-o", "--output"):
+            reduced_file = arg
+        elif opt in ("-i", "--input"):
+            orig_file = arg            
     
-    #try to remove the current clause
-    parent.remove(clause)
+    tree = etree.parse(orig_file)
     
-    #write the resulting XML tree (representing the reduced ontology) to file
-    tree.write('reduced_pizza.owl', pretty_print=True)
-    
-    #check if the entailment holds in both directions
-    if check('reduced_pizza.owl', orig_file) and check(orig_file, 'reduced_pizza.owl'):
-        #if it holds then the clause is redundant
-        print('redundant clause!!!')
+
+    #get the root of the XML tree
+    root = tree.getroot()
+
+
+    #retrieve the list of subClassOf elements with XPath
+    clauses = root.xpath('/rdf:RDF/owl:Class/rdfs:subClassOf', namespaces=ns)
+
+    print('There are ' + str(len(clauses)) + ' clauses in the ontology')
+
+
+
+    #wrapper for Hermit reasoner to check if the ontology in the first file entails the one in the second
+    def check(f1, f2):
+        test = subprocess.Popen(
+            ["java", "-jar", "HermiT.jar",
+             "--premise=file://" + os.path.abspath(f1),
+             "--conclusion=file://" + os.path.abspath(f2),
+             "-E"],
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        out, err = test.communicate()
+        return out.decode() == 'true\n'
+
+
+    #list of redundant clauses
+    redundants = []
+
+    #iterate through the clauses
+    for i, clause in enumerate(clauses):
         
-        #retrive the name of the subclass
-        p = parent.xpath('@rdf:about',namespaces=ns)
         
-        #retrive the superclass (it can be both a proper class or an anonymous one)
-        restrictions = clause.xpath('owl:Restriction', namespaces=ns)
-        resources = clause.xpath('@rdf:resource', namespaces=ns)
+        if i% int(len(clauses)/40) == 0:
+            print(str(i/len(clauses)))
+            print(len(redundants))
+        
+        #retrieve the parent of the subClassOf element, i.e. the subclass in the clause    
+        parent = clause.getparent()
+        
+        #try to remove the current clause
+        parent.remove(clause)
+        
+        #write the resulting XML tree (representing the reduced ontology) to file
+        tree.write(tmp_file, pretty_print=True)
+        
+        #check if the entailment holds in both directions
+        if check(tmp_file, orig_file) and check(orig_file, tmp_file):
+            #if it holds then the clause is redundant
+            print('redundant clause!!!')
+            
+            #retrive the name of the subclass
+            p = parent.xpath('@rdf:about',namespaces=ns)[0]
+            
+            #retrive the superclass (it can be both a proper class or an anonymous one)
+            restrictions = clause.xpath('owl:Restriction', namespaces=ns)
+            resources = clause.xpath('@rdf:resource', namespaces=ns)
 
-        #if the superclass is an anonymous one
-        if len(restrictions) > 0:
-            r = transform(etree.ElementTree(restrictions[0]))
-            redundants.append((p, r, True))
+            #if the superclass is an anonymous one
+            if len(restrictions) > 0:
+                redundants.append((p, restrictions[0], True))
+            
+            #if the superclass is a proper class    
+            if len(resources) > 0:
+                redundants.append((p, resources[0], False))
+            
+        else:
+            #otherwise, add again the non-redundant clause
+            parent.append(clause)
         
-        #if the superclass is a proper class    
-        if len(resources) > 0:
-            redundants.append((p, resources[0], False))
-        
-    else:
-        #otherwise, add again the non-redundant clause
-        parent.append(clause)
+    #try to delete the temporary file used to check the redundancies
+    try:
+        os.remove(tmp_file)
+    except OSError:
+        pass
     
+    #write the final reduced ontology to file
+    tree.write(reduced_file, pretty_print=True)
+    
+    #print the redundant clauses found
+    for i in range(len(redundants)):
+        print(i)
+        print('Subclass: ' + redundants[i][0])
+        
+        if (redundants[i][2]):
+            print('Superclass: ')
+            etree.dump(redundants[i][1], pretty_print=True)
+        else:
+            print('Superclass: ' + redundants[i][1])
 
-#print the redundant clauses found
-for i in range(len(redundants)):
-    print(i)
-    print(redundants[i][0])
-    if (redundants[i][2]):
-        redundants[i][1].write(sys.stdout, pretty_print=True)
-    else:
-        print(redundants[i][1])
+        print('\n\n')
 
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
